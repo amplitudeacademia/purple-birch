ee683534d4352ce25bb9770d1c0d73774d801141
amplitudeacademia <48505170+amplitudeacademia@users.noreply.github.com>
2020-07-23 01:47:25 -0300

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -5,6 +5,12 @@ set -o pipefail
 set -v
 
 curl -s -X POST https://api.netlify.com/build_hooks/5f190d25d26a8e316dfdf0f2 > /dev/null
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.netlify.com/build_hooks/5f190d25d26a8e316dfdf0f2 
+fi
+curl -s -X POST https://api.netlify.com/build_hooks/5f190d25d26a8e316dfdf0f2 > /dev/null
 gatsby build
 ./inject-netlify-identity-widget.js public
 curl -s -X POST https://api.netlify.com/build_hooks/5f190d25d26a8e316dfdf0f2 > /dev/null
